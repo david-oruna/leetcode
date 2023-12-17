bd69e042569107bbef891dca9a23a7a354d33d30
David H.O <104524650+david-oruna@users.noreply.github.com>
2023-12-17 00:01:23 -0500

    Update valid_anagram.py

--- files/solutions/valid_anagram.py
+++ files/solutions/valid_anagram.py
@@ -1,1 +1,22 @@
+#simple solutin
+def valid_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    return sorted(s) == sorted(t)
 
+
+# hash table 
+
+def valid_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    count = {}
+    for c in s:
+        count[c] = count.get(c, 0) + 1
+    for c in t:
+        if c not in count:
+            return False
+        count[c] -= 1
+        if count[c] < 0:
+            return False
+    return True
